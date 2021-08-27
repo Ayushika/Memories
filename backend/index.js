aab4ff8fcfb5211146d499fe52866361f461b746
Ayushika <ayushikabansal7890@gmail.com>
2021-08-27 22:59:22 +0530

    "Added Frontend"

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api running");
+});
+
+app.listen(5000, console.log("Server started"));
