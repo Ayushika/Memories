60b5e5178df0e8ff969707753f4213f03cad2969
Ayushika <ayushikabansal7890@gmail.com>
2021-08-27 23:49:37 +0530

    connected with database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 /** @format */
+import express from "express";
+import mongoose from "mongoose";
 
-const express = require("express");
 const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Api running");
 });
 
-app.listen(5000, console.log("Server started"));
+const CONNECTION_URL =
+  "mongodb+srv://ayushikaBansal:ayushikaBansal123@cluster0.tprhj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, console.log("Server started")))
+  .catch((error) => {
+    console.log(error.message);
+  });
